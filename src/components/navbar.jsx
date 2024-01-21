ab8abda9d005667f7a9e08e5d465fa998c991f88
Babatunde Ajibade <tunde4etin@yahoo.com>
2024-01-21 19:13:02 +0100

    fourth commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
+const NavBar = ({totalCounters}) => {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar{" "}
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    Navbar{" "}
+                    <span className="badge badge-pill badge-secondary">
+                        {totalCounters}
+                    </span>
+                </a>
+            </div>
+        </nav>
     );
-  }
-}
+};
+ 
 export default NavBar;
